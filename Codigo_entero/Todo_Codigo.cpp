7fb478d51257ab6f092c87bc586c0f23a51847a1
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-06-17 09:16:02 -0400

    Mandando cambios, añadieindo la declaracion de funciones.

--- Codigo_entero/Todo_Codigo.cpp
+++ Codigo_entero/Todo_Codigo.cpp
@@ -7,3 +7,17 @@
 
 using namespace std;//uso principal es para no tener el uso repetitivo de "std::"
 
+//Declaracion de variables globales
+map <string, set<string>> bd;
+
+//Declarar funciones 
+void pausar();
+bool validarFecha(const string& fecha, int& year, int& month, int& day);
+void agregarEvento(const string& fecha, const string& evento);
+void eliminarEvento(const string& fecha, const string& evento);
+void eliminarEventos(const string& fecha);
+void buscarEventoConFecha(const string& fecha);
+void imprimir();
+string obtenerPrimeraPalabra(string str);
+string eliminarPrimeraPalabra(string cadena);
+string obtenerLaFecha(string cadena);
