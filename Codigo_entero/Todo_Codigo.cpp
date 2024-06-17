b36c572f5abcf7a1eb41879e86e58153d9a77722
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-06-17 08:55:43 -0400

    inclusion de libreiras principales y namespace para evtiar el uso iterativo

---
+++ Codigo_entero/Todo_Codigo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <sstream>
+
+using namespace std;//uso principal es para no tener el uso repetitivo de "std::"
+
